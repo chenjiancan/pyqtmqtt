5383138250af989646beb02b0a982c847087b998
chenjiancan <chenjiancan@ubicell.com>
2017-07-07 12:07:45 +0800

    add username password field etc.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import random
 import sys
+import uuid
+from time import sleep
 
+import datetime
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import paho.mqtt.client as mqtt
@@ -11,6 +15,8 @@ from ui.ui_main_window import Ui_MainWindow
 # from src.mqttutils import *
 
 
+DEFAULT_HOST = "broker.mqttdashboard.com"
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     # class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -19,32 +25,96 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # self.ui.setupUi(self)
         self.setupUi(self)
 
+        self.checkBoxCleanSession.setChecked(True)
+        self.spinBoxKeepAlive.setValue(60)
+        self.pushButtonSub.setEnabled(False)
+        self.pushButtonPub.setEnabled(False)
+
+        self.lineEditHost.setText(DEFAULT_HOST)
+
+
         # mqtt client init
         self.is_connected = False
-        self.mqtt_client = MqttClient()
+        self.mqtt_client = None
 
+    def on_mqtt_msg(self, client, userdata, message:mqtt.MQTTMessage):
+        msg = message.payload.decode()
+        msg = str(datetime.datetime.now())[:-7] + ": " + message.topic + " == " + msg
+        print(msg)
+        self.textBrowserReceived.append(msg)
+
+    def on_mqtt_connect(self, client, userdata, flags, rc):
+        self.pushButtonSub.setEnabled(True)
+        self.pushButtonPub.setEnabled(True)
+
+    def on_mqtt_disconnect(self, client, userdata, flags, rc):
+        self.pushButtonSub.setEnabled(False)
+        self.pushButtonPub.setEnabled(False)
+
+
+    @pyqtSlot()
+    def on_pushButtonClientIdGenerate_clicked(self):
+        client_id = uuid.uuid4()
+        self.lineEditPortClientId.setText("qt_" + client_id.hex)
 
     @pyqtSlot(bool)
     def on_pushButtonConnect_clicked(self, para):
-        print("connecting to mqtt host: {}".format(self.lineEditHost.text()))
         host = self.lineEditHost.text()
         port = int(self.lineEditPort.text())
-        self.mqtt_client.loop_start()
-        self.mqtt_client.connect(host=host, port=port, keepalive=60)  # loop_start 先调用则阻塞， 后调用则不阻塞
-        print("is_connected", self.mqtt_client.is_connected)
+        print("connecting to mqtt host: {0}:{1}".format(host, port))
 
+        client_id = self.lineEditPortClientId.text()
+        keepalive = int(self.spinBoxKeepAlive.value())
+        clean_session = self.checkBoxCleanSession.isChecked()
+        auto_connect = self.checkBoxAutoConnect.isChecked()
+        username = self.lineEditPortUserName.text()
+        password = self.lineEditPortPassword.text()
+        #
+        # if self.mqtt_client.is_called_connect:
+        #     # self.mqtt_client.loop_stop()
+        #     self.mqtt_client.disconnect()
+
+        # self.mqtt_client = MqttClient(client_id=client_id,
+        #                               clean_session=clean_session,
+        #                               )
+        if self.mqtt_client:
+            self.mqtt_client.loop_stop()
+            self.mqtt_client.reinitialise(client_id=client_id, clean_session=clean_session)
+        else:
+            self.mqtt_client = MqttClient(client_id=client_id, clean_session=clean_session)
+            self.mqtt_client.on_message_handles.append(self.on_mqtt_msg)
+            self.mqtt_client.on_connect_handles.append(self.on_mqtt_connect)
+            self.mqtt_client.on_disconnect_handles.append(self.on_mqtt_disconnect)
+
+        # self.mqtt_client.tls_set("d:/tmp/cert")
+
+        try:
+            self.mqtt_client.username_pw_set(username=username, password=password)
+            self.mqtt_client.connect(host=host, port=port, keepalive=keepalive)  # loop_start 先调用则阻塞， 后调用则不阻塞
+            # self.mqtt_client.connect(host=host, port=8883, keepalive=keepalive)  # loop_start 先调用则阻塞， 后调用则不阻塞
+            self.mqtt_client.loop_start()
+        # print("is_connected", self.mqtt_client.is_connected)
+        except Exception as e:
+            print(e)
     @pyqtSlot()
     def on_pushButtonSub_clicked(self):
-        print("sub to mqtt topic: {}".format(self.lineEditTopicSub.text()))
-        topic = self.lineEditTopicSub.text()
-        self.mqtt_client.subscribe(topic=topic, qos=1)
+        if self.mqtt_client.is_connected:
+            print("sub to mqtt topic: {}".format(self.lineEditTopicSub.text()))
+            topic = self.lineEditTopicSub.text()
+            self.mqtt_client.subscribe(topic=topic, qos=1)
+        else:
+            print("sub failed, client is not connected")
 
     @pyqtSlot()
     def on_pushButtonPub_clicked(self):
-        print("pub to mqtt topic: {}".format(self.lineEditTopicPub.text()))
-        topic = self.lineEditTopicPub.text()
-        msg = self.lineEditMsgPub.text()
-        self.mqtt_client.publish(topic=topic, payload=msg.encode())
+        if self.mqtt_client.is_connected:
+            print("pub to mqtt topic: {}".format(self.lineEditTopicPub.text()))
+            topic = self.lineEditTopicPub.text()
+            msg = self.lineEditMsgPub.text()
+            self.mqtt_client.publish(topic=topic, payload=msg.encode())
+        else:
+            print("pub failed, client is not connected")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- src/mqttutils.py
+++ src/mqttutils.py
@@ -3,30 +3,63 @@ from time import sleep
 import paho.mqtt.client as mqtt
 from threading import Thread
 
+from copy import deepcopy, copy
+
 
 class MqttClient(mqtt.Client):
-    def __init__(self, *args, **kwargs):
-        super(MqttClient, self).__init__(*args, **kwargs)
+
+    def __init__(self, client_id="", clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp"):
+        super(MqttClient, self).__init__(client_id, clean_session, userdata, protocol, transport)
         self.is_connected = False
+
+        self.on_message_handles = []
+        self.on_connect_handles = []
+        self.on_disconnect_handles = []
 
         # The callback for when the client receives a CONNACK response from the server.
         def on_connect(client: mqtt.Client, userdata, flags, rc):
-            print("Connected with result code " + str(rc))
+            print("Connected with result code " + str(rc), flags)
             # Subscribing in on_connect() means that if we lose the connection and
             # reconnect then subscriptions will be renewed.
             # client.subscribe("$SYS/#")
-            self.is_connected = True
+            if rc == 0:
+                self.is_connected = True
+
+            for handle in self.on_connect_handles:
+                if callable(handle):
+                    handle(client, userdata, flags, rc)
+
+        def on_disconnect(client, userdata, rc):
+            self.is_connected = False
+            print("Disconnect from broker with result code : " + str(rc))
+            for handle in self.on_disconnect_handles:
+                if callable(handle):
+                    handle(client, userdata, rc)
 
         # The callback for when a PUBLISH message is received from the server.
         def on_message(client, userdata, msg: mqtt.MQTTMessage):
             print(msg.topic + " " + msg.payload.decode())
-
-        def on_disconnect(client, userdata, self):
-            print("disconnected!")
-            self.is_connected = False
+            for handle in self.on_message_handles:
+                if callable(handle):
+                    handle(client, userdata, msg)
 
         self.on_connect = on_connect
         self.on_message = on_message
         self.on_disconnect = on_disconnect
 
+
+    def reinitialise(self, *args, **kwargs):
+        try:
+
+            copy_on_message_handles = copy(self.on_message_handles)
+            copy_on_connect_handles = copy(self.on_connect_handles)
+            copy_on_disconnect_handles = copy(self.on_disconnect_handles)
+            super(MqttClient, self).reinitialise(*args, **kwargs)
+            # mqtt.Client.reinitialise(self, *args, **kwargs)
+
+            self.on_message_handles = copy_on_message_handles
+            self.on_connect_handles = copy_on_connect_handles
+            self.on_disconnect_handles = copy_on_disconnect_handles
+        except Exception as e:
+            print(e)
 
